e899b221a64526068f4c36b45f17ba622db77d21
WangShunda <wsdboy007@163.com>
2015-03-17 18:43:23 +0800

    20150317-1843

--- YumiTianqi/gen/com/boyumi/yumitianqi/R.java
+++ YumiTianqi/gen/com/boyumi/yumitianqi/R.java
@@ -20,49 +20,40 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
-        /** 
-         Per the design guidelines, navigation drawers should be between 240dp and 320dp:
-         https://developer.android.com/design/patterns/navigation-drawer.html
-    
-         */
-        public static final int navigation_drawer_width=0x7f040002;
     }
     public static final class drawable {
-        public static final int drawer_shadow=0x7f020000;
-        public static final int ic_drawer=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int background=0x7f020000;
+        public static final int cloudy=0x7f020001;
+        public static final int doge=0x7f020002;
+        public static final int fog=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int rain=0x7f020005;
+        public static final int snow=0x7f020006;
+        public static final int snowrain=0x7f020007;
+        public static final int sunny=0x7f020008;
+        public static final int sunnyrain=0x7f020009;
+        public static final int thunderstorm=0x7f02000a;
+        public static final int wind=0x7f02000b;
+        public static final int windrain=0x7f02000c;
     }
     public static final class id {
-        public static final int action_example=0x7f080008;
-        public static final int action_settings=0x7f080007;
-        public static final int button1=0x7f080005;
-        public static final int container=0x7f080001;
-        public static final int drawer_layout=0x7f080000;
-        public static final int editText1=0x7f080006;
-        public static final int navigation_drawer=0x7f080002;
-        public static final int section_label=0x7f080003;
-        public static final int textView1=0x7f080004;
+        public static final int CityName=0x7f080000;
+        public static final int Date=0x7f080004;
+        public static final int Temperature=0x7f080001;
+        public static final int WeatherCondition=0x7f080003;
+        public static final int WeatherIcon=0x7f080002;
+        public static final int action_settings=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
-        public static final int fragment_navigation_drawer=0x7f030002;
     }
     public static final class menu {
-        public static final int global=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_example=0x7f050006;
-        public static final int action_settings=0x7f050007;
+        public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int citycode_=0x7f050008;
-        public static final int getweather=0x7f050009;
-        public static final int navigation_drawer_close=0x7f050005;
-        public static final int navigation_drawer_open=0x7f050004;
-        public static final int title_section1=0x7f050001;
-        public static final int title_section2=0x7f050002;
-        public static final int title_section3=0x7f050003;
+        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
--- YumiTianqi/src/com/boyumi/yumitianqi/MainActivity.java
+++ YumiTianqi/src/com/boyumi/yumitianqi/MainActivity.java
@@ -1,166 +1,305 @@
 package com.boyumi.yumitianqi;
 
-import android.app.Activity;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.XmlResourceParser;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.support.v4.widget.DrawerLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+public class MainActivity extends Activity {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
+	LocationManager lm;
+	String Longitude;
+	String Latitude;
+	String CityName = "我不知道你在哪( E__E )";
+	String CityCode;
+	String WeatherCondition = "水生火热(→_→)";
+	String Temperature = "爆表啦(--)#";
+	String Date;
+	int WeatherCode;
+	int[] icons;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
-    private CharSequence mTitle;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		toShow();
+	}
+	
+	public void toShow(){
+		getGPSInfo();
+		getCityName();
+		getWeather();
+		int i = comfireWeatherIcon();
+		ImageView wi = (ImageView) findViewById(R.id.WeatherIcon);
+		wi.setImageResource(icons[i]);
+		System.out.println("finish");
+		TextView cn = (TextView) findViewById(R.id.CityName);
+		cn.setText(CityName);
+		TextView wc = (TextView) findViewById(R.id.WeatherCondition);
+		wc.setText(WeatherCondition);
+		TextView t = (TextView) findViewById(R.id.Temperature);
+		t.setText(Temperature);
+		TextView d = (TextView) findViewById(R.id.Date);
+		d.setText(Date);
+	}
+	// GPS location
+	public void getGPSInfo() {
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		Location locationGPS = lm
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		System.out.println(locationGPS);
+		Location locationPassive = lm
+				.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+		System.out.println(locationPassive);
+		Location locationNetwork = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		System.out.println(locationNetwork);
+		if(locationGPS!=null)updateLocation(locationGPS);
+		if(locationPassive!=null)updateLocation(locationPassive);
+		if(locationGPS==null&&locationPassive==null&&locationNetwork==null){
+			System.out.println("无法获取定位信息");
+			return;
+		}
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 8,
+				new LocationListener() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+					@Override
+					public void onStatusChanged(String provider, int status,
+							Bundle extras) {
+						// TODO Auto-generated method stub
+					}
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+					@Override
+					public void onProviderEnabled(String provider) {
+						// TODO Auto-generated method stub
+						updateLocation(lm.getLastKnownLocation(provider));
+					}
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
-    }
+					@Override
+					public void onProviderDisabled(String provider) {
+						// TODO Auto-generated method stub
+						updateLocation(null);
+					}
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        FragmentManager fragmentManager = getFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
-    }
+					@Override
+					public void onLocationChanged(Location location) {
+						// TODO Auto-generated method stub
+						updateLocation(location);
+					}
+				});
+	}
 
-    public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
-    }
+	// get cityName from baiduAPI
+	public void getCityName() {
+		final String baiduAPI_url = "http://api.map.baidu.com/geocoder?output=json&location="
+				+ Latitude+ "," + Longitude  + "&key=eZnlziqWIf9hYIokB4a4D7Bo";
+//		final String baiduAPI_url = "http://api.map.baidu.com/geocoder?output=json&location=31.9377,118.6386&key=eZnlziqWIf9hYIokB4a4D7Bo";
+		
+		Thread gcn = new Thread() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				super.run();
+				try {
+					DefaultHttpClient httpClient = new DefaultHttpClient();
+					HttpPost httpPost = new HttpPost(baiduAPI_url);
+					HttpResponse httpResponse = httpClient.execute(httpPost);
+					String strResult = EntityUtils.toString(httpResponse
+							.getEntity());
+					Log.i("cat", ">>>>>>" + strResult);
+					CityName = new JSONObject(strResult)
+							.getJSONObject("result")
+							.getJSONObject("addressComponent")
+							.getString("city");
+				} catch (JSONException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		gcn.start();
+		try {
+			gcn.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @SuppressWarnings("deprecation")
-	public void restoreActionBar() {
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
+	// get weather from yahoo
+	public void getWeather() {
+		CityCode = getcitycode("cityCode.txt", CityName);
+		final String temp_url = "http://weather.yahooapis.com/forecastrss?w="
+				+ CityCode + "&u=c";
+		Thread gw = new Thread() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				super.run();
+				try {
+					URLConnection con = (URLConnection) (new URL(temp_url))
+							.openConnection();
+					con.setRequestProperty("accept", "*/*");
+					con.setRequestProperty("connection", "Keep-Alive");
+					con.setRequestProperty("user-agent",
+							"Mozilla/4.0(compatible;MSIE 6.0;Windows NT 5.1;SV1)");
+					con.connect();
+					System.out.println("connected");
+					XmlPullParser parser = XmlPullParserFactory.newInstance()
+							.newPullParser();
+					parser.setInput(new InputStreamReader(con.getInputStream()));
 
+					while (parser.getEventType() != XmlResourceParser.END_DOCUMENT) {
+						if (parser.getEventType() == XmlResourceParser.START_TAG) {
+							String tagName = parser.getName();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
+							if (tagName.equals("yweather:condition")) {
+								System.out.println("pulling the weatherIfno");
+//								WeatherCondition = parser.getAttributeValue(
+//										null, "text");
+								Temperature = parser.getAttributeValue(null,
+										"temp");
+								Temperature += ("\u2103");
+								WeatherCode = Integer.valueOf(parser.getAttributeValue(null,
+										"code"));
+								Date = parser.getAttributeValue(null, "date");
+							}
+							
+						}
+						parser.next();
+					}
+				} catch (Exception e) {
+					System.out.println("error:" + e);
+				}
+			}
+		};
+		gw.start();
+		try {
+			gw.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public String getcitycode(String fileName, String cn) {
+		String citywhere = "你正在穿越虫洞？！";
+		try {
+			InputStream in = getResources().getAssets().open(fileName);
+			InputStreamReader read = new InputStreamReader(in, "UTF-8");
+			BufferedReader br = new BufferedReader(read);
+			String tempLine;
+			while ((tempLine = br.readLine()) != null) {
+				String[] tmpC = tempLine.split("\\|");
+				String[] tmpcn = tmpC[0].split(";");
+				if (cn.equals(tmpcn[0]) | cn.equals(tmpcn[1])) {
+					String citycode = tmpC[1];
+					System.out.println(citycode+";"+CityName+"find it!");				
+					return citycode;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return citywhere;
+	}
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
+	public void updateLocation(Location newLocation) {
+		if (newLocation != null) {
+			Longitude = String.valueOf(newLocation.getLongitude());
+			Latitude = String.valueOf(newLocation.getLatitude());
+		} else {
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
+		}
+	}
+	
+	public int comfireWeatherIcon(){
+		icons = new int[]{
+				R.drawable.windrain,
+				R.drawable.thunderstorm,
+				R.drawable.snowrain,
+				R.drawable.rain,
+				R.drawable.snow,
+				R.drawable.fog,
+				R.drawable.wind,
+				R.drawable.cloudy,
+				R.drawable.sunny,
+				R.drawable.doge,
+		};
+		switch(WeatherCode){
+		case 0:case 1:case 2:
+            WeatherCondition = "风暴";
+            return 0;
+        case 3:case 4:case 37:case 38:case 39:case 45:case 46:case 47:
+        	WeatherCondition = "雷阵雨";
+        	return 1;
+        case 5:case 6:case 7:case 35:case 17:
+        	WeatherCondition = "雨夹雪";
+        	return 2;
+        case 8:case 9:case 10:case 11:case 12:case 18:case 40:
+        	WeatherCondition = "阵雨";
+        	return 3;
+        case 13:case 14:case 15:case 16:case 41:case 42:case 43:
+        	WeatherCondition = "雪";
+        	return 4;
+        case 19:case 20:case 21:case 22:
+        	WeatherCondition = "雾";
+        	return 5;
+        case 23:case 24:case 25:
+        	WeatherCondition = "风";
+        	return 6;
+        case 26:case 27:case 28:case 29:case 30:case 44:
+        	WeatherCondition = "局部多云";
+        	return 7;
+        case 31:case 32:case 33:case 34:case 36:
+        	WeatherCondition = "晴";
+        	return 8;
+        default:
+        	return 9;
+		}
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-    
-    public void clickHandler(){
-    	EditText cc = (EditText) findViewById(R.id.editText1);
-    	int CC = cc.getVisibility();
-    	
-    }
-    public class CityResult {
-    	 
-        private String woeid;
-        private String cityName;
-        private String country;
-     
-        public CityResult() {}
-        public CityResult(String woeid, String cityName, String country) {
-            this.woeid = woeid;
-            this.cityName = cityName;
-            this.country = country;
-        }
-         // get and set methods
-        @Override
-        public String toString() {
-            return cityName + "," + country;
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
